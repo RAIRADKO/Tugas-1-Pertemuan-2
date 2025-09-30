4470f811d7dbb0569676cad01d1cabe1222c0e46
RAIRADKO <rahmat.purwatmoko@mhs.unsoed.ac.id>
2025-09-30 16:07:39 +0700

    Tugas 3 Pertemuan 4

---
+++ app/src/main/java/com/unsoed/informatikamobile/data/model/SearchResponse.kt
@@ -0,0 +1,16 @@
+package com.unsoed.informatikamobile.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+    @SerializedName("title")
+    val docs:List<BookDoc>
+)
+data class BookDoc(
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("author_name")
+    val authorName:List<String>?,
+    @SerializedName("first_publish_year")
+    val firstPublishYear: Int?
+)
